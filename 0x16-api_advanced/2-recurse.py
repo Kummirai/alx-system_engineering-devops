31c961c73fac0aaea6f93ed5348e7f0607dade81
Kummirai <ajaxmilton@hotmail.com>
2023-08-09 00:33:58 +0200

    Fix recurse code

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,43 +1,30 @@
 #!/usr/bin/python3
-"""
-Using reddit's API
-"""
+"""Function to query a list of all hot posts on a given Reddit subreddit."""
 import requests
 
 
-def count_words(subreddit, word_list, found_list=[], after=None):
-    """Printing a sorted count of article titles"""
+def recurse(subreddit, hot_list=[], after="", count=0):
+    """Returns a list of titles of all hot posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        return None
 
-    headers = {'User-Agent': 'api_advanced-project'}
-    url = "https://www.reddit.com/r/{}/hot.json?after={}"\
-        .format(subreddit, after)
-    parameters = {'after': after}
-    res = requests.get(url, params=parameters, headers=headers,
-                       allow_redirects=False)
+    results = response.json().get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        hot_list.append(c.get("data").get("title"))
 
-    if after is None:
-        word_list = [word.lower() for word in word_list]
-
-    if res.status_code == 200:
-        res = res.json()['data']
-        aft = res['after']
-        res = res['children']
-        for item in res:
-            title = item['data']['title'].lower()
-            for word in title.split(' '):
-                if word in word_list:
-                    found_list.append(word)
-        if aft is not None:
-            count_words(subreddit, word_list, found_list, aft)
-        else:
-            result = {}
-            for word in found_list:
-                if word.lower() in result.keys():
-                    result[word.lower()] += 1
-                else:
-                    result[word.lower()] = 1
-            for key, value in sorted(result.items(), key=lambda item: item[1],
-                                     reverse=True):
-                print('{}: {}'.format(key, value))
-    else:
-        return
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
+    return hot_list
