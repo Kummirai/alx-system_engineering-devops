0549612b51d3bd5e939b22df48b6565fc51ae7eb
Kummirai <ajaxmilton@hotmail.com>
2023-08-09 00:29:01 +0200

    Fix code

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,28 +1,43 @@
 #!/usr/bin/python3
-
+"""
+Using reddit's API
+"""
 import requests
-after = None
 
 
-def recurse(subreddit, hot_list=[]):
-    """
-    returning top ten post titles recursively
-    """
-    global after
+def count_words(subreddit, word_list, found_list=[], after=None):
+    """Printing a sorted count of article titles"""
+
     headers = {'User-Agent': 'api_advanced-project'}
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json?after={}"\
+        .format(subreddit, after)
     parameters = {'after': after}
     res = requests.get(url, params=parameters, headers=headers,
                        allow_redirects=False)
 
+    if after is None:
+        word_list = [word.lower() for word in word_list]
+
     if res.status_code == 200:
-        next_ = res.json().get("data").get("after")
-        if next_ is not None:
-            after = next_
-            recurse(subreddit, hot_list)
-        all_titles = res.json().get("data").get("children")
-        for title_ in all_titles:
-            hot_list.append(title_.get("data").get("title"))
-        return hot_list
+        res = res.json()['data']
+        aft = res['after']
+        res = res['children']
+        for item in res:
+            title = item['data']['title'].lower()
+            for word in title.split(' '):
+                if word in word_list:
+                    found_list.append(word)
+        if aft is not None:
+            count_words(subreddit, word_list, found_list, aft)
+        else:
+            result = {}
+            for word in found_list:
+                if word.lower() in result.keys():
+                    result[word.lower()] += 1
+                else:
+                    result[word.lower()] = 1
+            for key, value in sorted(result.items(), key=lambda item: item[1],
+                                     reverse=True):
+                print('{}: {}'.format(key, value))
     else:
-        return (None)
+        return
