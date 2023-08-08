fd5235e1f0827a2b770decf96740f3b916e3b9da
Kummirai <ajaxmilton@hotmail.com>
2023-08-09 00:26:00 +0200

    Fix comment

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""
-Using reddit's API
-"""
+
 import requests
 after = None
 
 
 def recurse(subreddit, hot_list=[]):
-    """returning top ten post titles recursively"""
+    """
+    returning top ten post titles recursively
+    """
     global after
     headers = {'User-Agent': 'api_advanced-project'}
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
